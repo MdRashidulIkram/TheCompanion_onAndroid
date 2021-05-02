c8c46a43604309350ea7818b8842208cc3a98c18
Farshed Adib <farshedadib1998422@gmail.com>
2021-05-02 17:00:52 -0400

    ok

--- app/src/main/java/com/example/the_companion/PrimaryDashboard.java
+++ app/src/main/java/com/example/the_companion/PrimaryDashboard.java
@@ -2,13 +2,18 @@ package com.example.the_companion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PrimaryDashboard extends AppCompatActivity {
+Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_primary_dashboard);
+
+        nextTaskList();
     }
+    private void nextTaskList()
 }
