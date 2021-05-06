63d9038c39a3f387f6b93fd29ab24aa8dd881fec
md rashidul sumy <rashidulikram406@gmail.com>
2021-05-05 20:54:53 -0400

    login firebase

--- app/src/main/java/com/example/the_companion/login_screen.java
+++ app/src/main/java/com/example/the_companion/login_screen.java
@@ -1,24 +1,38 @@
 package com.example.the_companion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class login_screen extends AppCompatActivity {
 
     //variables
     Animation bottomAnim;
     View view2;
-    Button button3, button4;
-    TextInputLayout textInputLayout, textInputLayout3;
+    Button btnSignIn, btnSignUp;
+    EditText email, password;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -27,32 +41,70 @@ public class login_screen extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_screen);
 
-        configureRegButton();
+        nextPrimaryDashboard();
 
+        mAuth = FirebaseAuth.getInstance();
         //animations
         bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
 
         //Hooks
         view2 = findViewById(R.id.view2);
-        button3 = findViewById(R.id.sign_in_button);
-        button4 = findViewById(R.id.sign_up_button);
-        textInputLayout = findViewById(R.id.textInputLayout);
-        textInputLayout3 = findViewById(R.id.textInputLayout3);
+        btnSignIn = findViewById(R.id.sign_in_button);
+        btnSignUp = findViewById(R.id.sign_up_button);
+        email = findViewById(R.id.textBoxEmail);
+        password = findViewById(R.id.textBoxPassword);
+
         view2.setAnimation(bottomAnim);
-        button3.setAnimation(bottomAnim);
-        button4.setAnimation(bottomAnim);
-        textInputLayout.setAnimation(bottomAnim);
-        textInputLayout3.setAnimation(bottomAnim);
+        btnSignIn.setAnimation(bottomAnim);
+        btnSignUp.setAnimation(bottomAnim);
+        email.setAnimation(bottomAnim);
+        password.setAnimation(bottomAnim);
+
+        btnSignIn.setOnClickListener(v -> {
+
+            loginUser();
+        });
     }
 
-    private void configureRegButton(){
+    private void loginUser(){
+        String emailText = email.getText().toString();
+        String passText = password.getText().toString();
 
-        Button regButton = (Button) findViewById(R.id.sign_up_button);
-        regButton.setOnClickListener(new View.OnClickListener() {
+
+        if (!emailText.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
+            if (!passText.isEmpty()){
+                mAuth.signInWithEmailAndPassword(emailText , passText)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(login_screen.this, "Login Successfully !!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(login_screen.this, PrimaryDashboard.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(login_screen.this, "Login Failed !!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }else{
+                password.setError("Empty Fields Are not Allowed");
+            }
+        }else if(emailText.isEmpty()){
+            email.setError("Empty Fields Are not Allowed");
+        }else{
+            email.setError("Pleas Enter Correct Email");
+        }
+    }
+
+    private void nextPrimaryDashboard(){
+        Button nextbtn = findViewById(R.id.sign_up_button);
+        nextbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(login_screen.this, registration_screen.class));
             }
         });
     }
+
 }
